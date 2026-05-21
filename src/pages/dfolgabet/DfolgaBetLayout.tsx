01c00b9e177d713e58902abd4fd2945832fc06ff
EricoGomes42 <50212915+EricoGomes42@users.noreply.github.com>
2026-05-21 15:56:47 -0300

    Temporarily disable adblock modal

--- src/pages/dfolgabet/DfolgaBetLayout.tsx
+++ src/pages/dfolgabet/DfolgaBetLayout.tsx
@@ -21,7 +21,6 @@ export default function DfolgaBetLayout() {
       <DfolgaBetFooter />
       <CookieConsentModal />
       <AgeVerificationModal />
-      <AdBlockWarningModal />
     </div>
   );
 }
